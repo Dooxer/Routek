7ef1da58bf2b166fe9581e1ef470bec635e56f6d
KristjanJesih <kristo.kisli@gmail.com>
2021-05-22 21:16:16 +0200

    Rebuild and Add camera

--- Android/app/src/main/java/com/example/routek/MainActivity.java
+++ Android/app/src/main/java/com/example/routek/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.routek;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 
@@ -17,10 +24,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.CAMERA
+            }, 100);
+        }
     }
 
     public void openSignRecognitionActivity(View view) {
-        Intent intent = new Intent(this, SignRecognitionActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, SignRecognitionActivity.class);
+//        startActivity(intent);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+        }
     }
 }
